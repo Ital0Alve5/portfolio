6d55922fee65714e280a82795ccf72a2b3bc3819
Ital0Alve5 <italo_alves_@outlook.com>
2021-03-17 13:22:13 -0300

    Consertado bug do form de contato.

--- public/js/contato.js
+++ public/js/contato.js
@@ -1,39 +1,37 @@
-window.addEventListener('scroll', ()=>{
-const div = document.querySelector('.contato')
-if(window.innerWidth > 500){
-        if(div.getBoundingClientRect().top < innerHeight-((24*innerHeight)/100)+100){
-        new Promise((res)=>{
-        div.style.marginLeft = '50%'
-        div.style.transform = 'translateX(-50%)'
-        res()
-     })
-     .then(()=>{
-          const intervalo = setInterval(()=>{
-              if(div.getBoundingClientRect().x > window.innerWidth/3){
-                  div.style.height = '39vw'
-                  clearInterval(intervalo)
-                }
+window.addEventListener('scroll', contatoDeslize)
+function contatoDeslize(){
+    if(window.innerWidth > 500){
+        const div = document.querySelector('.contato')
+            if(div.getBoundingClientRect().top < innerHeight-((24*innerHeight)/100)+100){
+            new Promise((res)=>{
+            div.classList.add('contatoDoneTranslate')
+            res()
+         })
+         .then(()=>{
+              const intervalo = setInterval(()=>{
+                  if(div.getBoundingClientRect().x > window.innerWidth/3){
+                    div.classList.add('contatoDoneDimensions')
+                      clearInterval(intervalo)
+                    }
+                })
             })
-        })
+        }
     }
-}
-else{
-    if(div.getBoundingClientRect().top <  innerHeight-((24*innerHeight)/100)+100){
-        new Promise((res)=>{
-        div.style.marginLeft = '50%'
-        div.style.transform = 'translateX(-50%)'
-        res()
-     })
-     .then(()=>{
-          const intervalo = setInterval(()=>{
-                  div.style.height = '100%'
-                  div.style.width = '100%'
-                  clearInterval(intervalo)
+    else{
+        if(div.getBoundingClientRect().top <  innerHeight-((24*innerHeight)/100)+100){
+            new Promise((res)=>{
+            div.classList.add('contatoDoneTranslate')
+            res()
+         })
+         .then(()=>{
+              const intervalo = setInterval(()=>{
+                    div.classList.add('contatoDoneDimensions')
+                      clearInterval(intervalo)
+                })
             })
-        })
+        }
     }
-}
-})
+    }
 
 const submit = document.querySelector('.enviar')
 let msgCount = 0
